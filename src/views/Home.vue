8b929ff5e9a6d2bba1c07595f1767d2e6478cbae
Souris.CLOUD <me@souris.cloud>
2021-01-04 22:15:19 +0100

    warnings update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <v-card-text>
     <p>Your account informations stays secure! Application has been updated to dont reveal your access token in link, so its now safe to make a screenshot or whatever.</p>
     <p>Service used to fetch informations about your account from Riot API is just used as proxy service and doesn't store your account information at all!</p>
+    <p>This application has nothing to do with Riot itself, it just using public shared code to replicate Riot RSO to get your MMR updates.</p>
     <p>Sources (proof of security): <ul>
       <li><a href="https://github.com/souriscloud/valorant-elo">this page (github source)</a></li>
       <li><a href="https://github.com/souriscloud/valoments-backend/blob/master/src/services/valoleak/valoleak.class.js">/valoleak endpoint of api.valoments.souris.cloud (service source code)</a></li>
