890b6b777d1c219a6f7e746f6c1ce1892338638a
Souris.CLOUD <me@souris.cloud>
2021-01-05 20:31:22 +0100

    get rid of old garbage (riot logic to backend)

--- src/riot/valoformat.js
+++
@@ -1,7 +0,0 @@
-/*
-
-  const lastMatch = matches[0]
-  const lastUpdatePlus = lastMatch.TierProgressAfterUpdate > lastMatch.TierProgressBeforeUpdate
-  const lastEloUpdate = lastUpdatePlus ? lastMatch.TierProgressAfterUpdate - lastMatch.TierProgressBeforeUpdate : lastMatch.TierProgressBeforeUpdate - lastMatch.TierProgressAfterUpdate
-
-*/
