d3f37e342cbbd1a33fe2df300ed912132074c14e
Souris.CLOUD <me@souris.cloud>
2021-01-08 04:17:52 +0100

    deletion

--- src/views/Elo.vue
+++ src/views/Elo.vue
@@ -5,7 +5,7 @@
       <v-spacer></v-spacer>
       <LangSwitch />
       <v-spacer></v-spacer>
-      <v-dialog v-model="shareDialog" width="500">
+      <v-dialog v-model="shareDialog" width="700">
         <template v-slot:activator="{ on, attrs }">
           <v-btn dark v-bind="attrs" v-on="on" x-large>SHARE</v-btn>
         </template>
@@ -13,9 +13,9 @@
         <v-card>
           <v-card-title>Share (or update existing) competitive progress card.</v-card-title>
           <template v-if="!isSharing">
-            <v-card-subtitle>Since we don't store your passwords, you have to update them with SHARE button yourself anytime you want it update!</v-card-subtitle>
+            <v-card-text>Since we don't store your passwords, you have to update them with SHARE button yourself anytime you want it update!</v-card-text>
             <v-card-text>When you share your competitive progress card, static link will be generated and will be totally public for all that have the link, there is no account sensitive data, just numbers about your progress, and if you want, you can request deletion, when you open your static link and click the button "REQUEST DELETION", you will authorize by your riot account and static link will be deleted forever!</v-card-text>
-            <v-card-text>Czech translation and requestion deletion will be available in one or two days, or you can just reach me thru github!</v-card-text>
+            <v-card-text>Czech translation will be available in one or two days!</v-card-text>
             <v-card-actions>
               <v-btn
                 dark
--- src/views/Shared.vue
+++ src/views/Shared.vue
@@ -15,18 +15,6 @@
         <v-col :cols="loaded ? 11 : 12">
           <v-toolbar-title>{{ title }}</v-toolbar-title>
         </v-col>
-        <!-- <template v-if="loaded">
-          <v-col cols="2">
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn v-on="on" v-bind="attrs" @click="reload">
-                  <v-icon>mdi-trash</v-icon>
-                </v-btn>
-              </template>
-              <span>{{ $t('reload') }}</span>
-            </v-tooltip>
-          </v-col>
-        </template> -->
       </v-row>
     </v-toolbar>
     <v-card-text>
@@ -58,6 +46,46 @@
         <v-progress-linear indeterminate dark />
       </template>
     </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <LangSwitch />
+      <v-spacer></v-spacer>
+      <v-dialog v-model="deletionDialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn dark v-bind="attrs" v-on="on" x-large>REQUEST DELETION</v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title>Competitive progress card deletion</v-card-title>
+          <template v-if="!deletionProgress">
+            <v-card-text>When you delete your competitive progress card, this static link will be destroyed forever. You can make then new share and generate new link!</v-card-text>
+            <v-card-text>Credentials are again send to Riot to authorize hash of your profile ID, that you have stored/shared this competitive progress card, it has nothing to do with Valorant itself.</v-card-text>
+            <v-card-text>
+              <v-text-field v-model="deletionCreds.username" label="username" />
+              <v-text-field v-model="deletionCreds.password" label="password" />
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                dark
+                @click="deletionDialog = false"
+              >
+                Don't delete
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn
+                dark
+                @click="deletion"
+              >
+                Delete
+              </v-btn>
+            </v-card-actions>
+          </template>
+          <template v-else>
+            <v-progress-linear dark indeterminate />
+          </template>
+        </v-card>
+      </v-dialog>
+    </v-card-actions>
   </div>
 </template>
 
@@ -66,11 +94,13 @@ import axios from 'axios'
 import { endpoint } from '@/apilink.json'
 import Rank from '@/components/Rank.vue'
 import MatchCard from '@/components/MatchCard.vue'
+import LangSwitch from '@/components/LangSwitch.vue'
 
 export default {
   components: {
     Rank,
-    MatchCard
+    MatchCard,
+    LangSwitch
   },
 
   computed: {
@@ -95,7 +125,13 @@ export default {
       noRanked: null,
       lastMatch: null,
       loaded: false,
-      updatedAt: null
+      updatedAt: null,
+      deletionDialog: false,
+      deletionProgress: false,
+      deletionCreds: {
+        username: '',
+        password: ''
+      }
     }
   },
 
@@ -103,19 +139,44 @@ export default {
     home () {
       this.$store.dispatch('clear')
       this.$router.push('/')
+    },
+
+    async deletion () {
+      this.deletionProgress = true
+      const { username, password } = this.deletionCreds
+      this.deletionCreds.username = ''
+      this.deletionCreds.password = ''
+      if (username !== '' && password !== '' && this.userId !== '') {
+        const response = await axios.post(endpoint, {
+          type: 'card-del-req',
+          username,
+          password,
+          userId: this.userId
+        })
+        this.deletionProgress = false
+        this.deletionDialog = false
+
+        if (response.data) {
+          this.$router.push('/')
+        }
+      }
     }
   },
 
   async mounted () {
-    await this.$store.dispatch('clearData')
-    const response = await axios.get(`${endpoint}-compet-cards/${this.$route.params.id}`)
-    if (response.data) {
-      this.userId = response.data.userId
-      this.userInfo = response.data.userInfo
-      this.matches = response.data.matches
-      this.noRanked = response.data.noRanked
-      this.lastMatch = response.data.lastMatch
-      this.updatedAt = response.data.updatedAt
+    await this.$store.dispatch('clear')
+    if (!this.$route.params.id) {
+      this.$router.push('/')
+    } else {
+      const response = await axios.get(`${endpoint}-compet-cards/${this.$route.params.id}`)
+      if (response.data) {
+        this.userId = response.data.userId
+        this.userInfo = response.data.userInfo
+        this.matches = response.data.matches
+        this.noRanked = response.data.noRanked
+        this.lastMatch = response.data.lastMatch
+        this.updatedAt = response.data.updatedAt
+      }
     }
     this.loaded = true
   }
